5bc30fdb564f8622299a1bcb91f97529edf487e8
amarnarute <amarnarute2002@gmail.com>
2021-08-05 19:49:29 +0530

    Style: CSS trianle for landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <div className={classes.container}>
+            <div className={classes['top-arrow']}>
+              <Link to="/top">
+                <div className={classes.top}></div>
+              </Link>
+            </div>
+
+            <div className={classes['bottom-arrows']}>
+              <Link to="/left">
+                <div className={classes.left}></div>
+              </Link>
+              <Link to="/center">
+                <div className={classes.center}></div>
+              </Link>
+              <Link to="/right">
+                <div className={classes.right}></div>
+              </Link>
+            </div>
+          </div>
+        </nav>
+
+        {/* <Link to="/">Home</Link> */}
+        {/* <Switch>
+          <Route path="/right">
+            <Right />
+          </Route>
+          <Route path="/top">
+            <Top />
+          </Route>
+          <Route path="/center">
+            <Center />
+          </Route>
+          <Route path="/left">
+            <Left />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch> */}
+      </div>
+    </Router>
   );
 }
 
